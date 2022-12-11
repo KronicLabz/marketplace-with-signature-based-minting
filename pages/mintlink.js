5da75e0a1be93e0c61daa5c8aaf750ab0d17f406
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-12-11 13:46:07 -0600

    Update mintlink.js

--- pages/mintlink.js
+++ pages/mintlink.js
@@ -52,7 +52,7 @@ const Links = () => {
                     >
                         <Link href="https://mint.boredapepixelclub.net/" passHref>
                             <img
-                                src={`/images/BAPC.png`}
+                                src={`/images/bapc.png`}
                                 className={styles.featuredImage}
                                 width={1250}
                                 height={750}
